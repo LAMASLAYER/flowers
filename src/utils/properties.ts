d34a618e2c8bdf32ae0c7d8ab72d6804a6c841c3
ku714 <jerome.wilmart@clearstream.com>
2018-09-21 16:50:33 +0200

    uploader

---
+++ src/utils/properties.ts
@@ -0,0 +1,4 @@
+export class Properties {
+  public static server = 'http://springpimence.herokuapp.com';
+  public static CLOUDINARY_URL = 'cloudinary://272379686864837:mufgDqRpgFROp50P8AIRM-92ZCA@fleurslesale';
+}
