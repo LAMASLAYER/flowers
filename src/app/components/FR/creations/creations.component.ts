42544ad2a5f9438d905547da61878498657fc928
LAMASLAYER <jerome.wilmart@gmail.com>
2019-06-01 16:44:43 +0200

    fixes

--- src/app/components/FR/creations/creations.component.ts
+++ src/app/components/FR/creations/creations.component.ts
@@ -3,6 +3,8 @@ import {Router} from '@angular/router';
 import {Contact} from '../../../models/contact';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {map} from 'rxjs/operators';
+import {HttpClient} from "@angular/common/http";
+import {Assets} from "../../../models/assets";
 
 @Component({
   selector: 'app-creations',
@@ -11,10 +13,13 @@ import {map} from 'rxjs/operators';
 })
 export class CreationsComponent implements OnInit {
   router: Router;
-  newMail: Contact;
   public menu: boolean;
-  public barcelone: boolean;
-  public mariage: boolean;
+  private http: HttpClient;
+  public weddingUrl: string;
+  public funeralsUrl: string;
+  public weddingOrientation: string;
+  public funeralsOrientation: string;
+  public orientationRequest: string;
 
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
@@ -31,21 +36,44 @@ export class CreationsComponent implements OnInit {
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver, router: Router) {
+  constructor(private breakpointObserver: BreakpointObserver, router: Router, http: HttpClient) {
     this.router = router;
-    this.newMail = new Contact();
+    this.http = http;
   }
   ngOnInit() {
-    this.menu = false;
-    this.mariage = true
-  }
-  goTo(url: string) {
-    this.router.navigate(['fr/dashboard']);
+    this.http.get('https://pathfinderappfinder.herokuapp.com/assets/category/wedding').subscribe(
+      (data: Array<Assets>) => {
+        console.log(data);
+        const randomData = data[Math.floor(Math.random() * data.length)];
+        console.log(randomData);
+        this.weddingUrl =  randomData.url;
+        this.weddingOrientation = randomData.orientation;
+        if(this.weddingOrientation == 'portrait') {
+          this.orientationRequest = 'portrait'
+        }else {
+          this.orientationRequest = 'paysage'
+        }
+        console.log(this.orientationRequest);
+        this.http.get('https://pathfinderappfinder.herokuapp.com/assets/category/funerals/orientation/' + this.orientationRequest).subscribe(
+          (data2: Array<Assets>) => {
+            const randomData2 = data2[Math.floor(Math.random() * data2.length)];
+            this.funeralsUrl = randomData2.url;
+            this.funeralsOrientation = randomData2.orientation;
+          }
+        );
+      }
+    );
+
   }
 
-  public english() {
-    localStorage.setItem('fllang', 'en');
-    this.router.navigate(['en/creations']);
+  goTo(url: string) {
+    this.router.navigate([url]);
   }
+
+  public french() {
+    localStorage.setItem('fllang', 'fr');
+    this.router.navigate(['fr/creations']);
+  }
+
 
 }
