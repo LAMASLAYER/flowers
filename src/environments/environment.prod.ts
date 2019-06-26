ce46b82a39a4d60c8ae7e175a6a20f0bd421f928
Jérôme Wilmart <jerome.wilmart@gmail.com>
2019-06-26 08:09:31 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true
+  firebase: {
+    apiKey: "AIzaSyAXapbjFS3hgk36gDl3SItJx63aRKbvDS8",
+    authDomain: "fleurlesale.firebaseapp.com",
+    databaseURL: "https://fleurlesale.firebaseio.com",
+    projectId: "fleurlesale",
+    storageBucket: "fleurlesale.appspot.com",
+    messagingSenderId: "1070162181370",
+    appId: "1:1070162181370:web:349d157c49993f17"
+  }
 };
